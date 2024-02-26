f072c727ae89d355b79a4598a91a2eb54aa9bd92
mastercodees <vladmirnyataige@gmail.com>
2024-02-26 21:27:15 +0300

    completed

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -5,21 +5,38 @@ print("welcome",name,"to this adventure")
 answer = input("You are on a dirt road, it has  come to an end and you can go left or right. which way would you like to go? ").lower()
 
 if answer == "left":
-    answer = input("You come to a river, you can walk around it or swim across? type walk to walk around and swim to swim  across: )  ")
+    answer = input("You come to a river, you can walk around it or swim across? type walk to walk around and swim to swim  across: ")
     if answer == "swim":
       
-      print()
+      print("you swam across and was eaten by an allegator.")
 
     elif answer == "walk":
 
-        print()
+        print("you walked for many miles, ran out of water and you lost the game")
     else:
         print("Not a valid choice you loose.")
 
 elif answer == "right":
 
-    print()
+    answer = input("you have come to a bridge it looks wabbly, do you want to cross it or not (cross/back)")
+    if answer == "back":
+      
+      print("you go back and lose.")
+
+    elif answer == "cross":
+
+        answer = input("you cross the bridge and meet a stranger. Do ypu talk to them (yes/no)? ")
+        if answer == "yes":
+            print("You talk to the stranger and they give you gold. you WIN!")
+        elif answer == "no":
+            print("You ignore the stranger and they are offended. YOU LOOSE")
+        else:
+            print("Not a valid choice you loose.")
+    else:
+        print("Not a valid choice you loose.")
 
 else:
 
     print("Not a valid choice you loose.")
+
+print("Thank you for trying", name)
